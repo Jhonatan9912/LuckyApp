5f8bb7c36cc298fef2798fc92273a12c69d17e67
Jhonatan9912 <Jhonathanestiven1999@gmail.com>
2025-08-27 08:59:19 -0500

    cambios en  reset9

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -4,7 +4,4 @@ import os
 bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"  # Railway inyecta PORT
 workers = int(os.environ.get('WEB_CONCURRENCY', '2'))
 timeout = 120
-loglevel = "debug"      # nivel máximo de detalle
-capture_output = True   # 👈 fuerza que stdout/stderr vayan a logs
-accesslog = "-"         # 👈 imprime access log en consola
-errorlog = "-"          # 👈 imprime errores en consola
+loglevel = "debug"
